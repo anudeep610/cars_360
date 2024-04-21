1a54cd2048412d0e89a1e1781bb47f5099b1b874
Anudeep <anudeep.hanuman@gmail.com>
2024-04-21 16:28:10 +0530

    added payment page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import AddCar from "./components/addCar/addCar";
 import ViewListings from "./components/viewListings/viewListings";
 import ViewBookings from "./components/viewBookings/viewBookings";
 import ViewAvailable from "./components/viewAvailable/viewAvailable";
+import Payment from "./components/payment/payment";
 import BookCar from "./components/bookCar/bookCar";
 import Login from "./components/login/login";
 import Signup from "./components/signup/signup";
@@ -18,6 +19,7 @@ export default function App() {
   const [user, setUser] = useState({});
 
   const [carToBeBooked, setCarToBeBooked] = useState({});
+  const [bookingData, setBookingData ] = useState({});
 
   return (
     <BrowserRouter>
@@ -30,7 +32,8 @@ export default function App() {
           <Route path="/view-listings" element={<ViewListings/>} />
           <Route path="/view-bookings" element={<ViewBookings/>} />
           <Route path="/view-available" element={<ViewAvailable setCarToBeBooked={setCarToBeBooked}/>} />
-          <Route path="/view-available/:carId" element={<BookCar carToBeBooked={carToBeBooked}/>} />
+          <Route path="/view-available/:carId" element={<BookCar setBookingData={setBookingData} carToBeBooked={carToBeBooked}/>} />
+          <Route path="/book-car/payment/:carId" element={<Payment bookingData={bookingData}/>} />
         </Routes>
       </UserContext.Provider>
     </BrowserRouter>
